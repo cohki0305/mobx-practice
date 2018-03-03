fdd84aa07250eb7ed63f07ce3edf3c1b9096c7c3
cohki0305 <cohki0305@gmail.com>
2018-03-03 14:59:00 +0900

    use local storage

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './assets/index.css';
 import App from './components/App';
 import { WishList } from './models/WishList'
+import { onSnapshot } from 'mobx-state-tree'
 
-const wishList = WishList.create({
+let initialState = {
   items: [
     {
       name: "LEGO Mindstorms EV3",
@@ -17,6 +18,17 @@ const wishList = WishList.create({
       image: "https://images-na.ssl-images-amazon.com/images/I/51a7xaMpneL._SX329_BO1,204,203,200_.jpg"
     }
   ]
-})
+}
+const wishListStorage = localStorage.getItem('wishlistapp')
+if (wishListStorage) {
+  const json = JSON.parse(wishListStorage)
+  if (WishList.is(json)) initialState = json
+}
+
+const wishList = WishList.create(initialState)
 
 ReactDOM.render(<App wishList={wishList}/>, document.getElementById('root'));
+
+onSnapshot(wishList, snapshot => {
+  localStorage.setItem('wishlistapp', JSON.stringify(snapshot))
+})
