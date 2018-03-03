92a802e86987a5ab2e8e7911243ee53bce789764
cohki0305 <cohki0305@gmail.com>
2018-03-03 15:17:37 +0900

    restore

--- src/components/WishListItemView.js
+++ src/components/WishListItemView.js
@@ -17,7 +17,7 @@ class WishListItemView extends Component {
       <li className="item">
         {item.image && <img src={item.image} />}
         <h3>{item.name}</h3>
-        <span>{item.price}</span>
+        <span>{item.price} €</span>
         <span>
           <button onClick={this.onToggleEdit}>Edit</button>
           <button onClick={item.remove}>Remove</button>
