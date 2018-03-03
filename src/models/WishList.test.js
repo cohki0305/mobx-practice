efab2c8f4a0a2191f6af5fde8452a225702a74fc
cohki0305 <cohki0305@gmail.com>
2018-03-03 10:18:08 +0900

    learn about snapshot test

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,4 +1,5 @@
 import { WishListItem, WishList } from './WishList'
+import { getSnapshot, onSnapshot, onPatch } from 'mobx-state-tree'
 
 it('can create a instance of a model', () => {
   const item = WishListItem.create({
@@ -26,8 +27,30 @@ it('can create a wishlist', () => {
   expect(list.items.length).toBe(1)
 })
 
+it("can add new items - 2", () => {
+  const list = WishList.create()
+  const patches = []
+  onPatch(list, patch => {
+    patches.push(patch)
+  })
+
+  list.add({
+    name: "Chesterton",
+    price: 10
+  })
+
+  list.items[0].changeName("Book of G.K. Chesterton")
+
+  expect(patches).toMatchSnapshot()
+})
+
 it('can add new items', () => {
   const list = WishList.create()
+  const states = []
+  onSnapshot(list, snapshot => {
+    states.push(snapshot)
+  })
+
   const item = WishListItem.create({
     name: 'Chesterton',
     price: 10
@@ -39,4 +62,10 @@ it('can add new items', () => {
 
   list.items[0].changeName('Book of G.K. Chesterton')
   expect(list.items[0].name).toBe('Book of G.K. Chesterton')
+
+  // getSnapshot => return pure json
+  // snapshot is immutable, copy
+  expect(getSnapshot(list)).toMatchSnapshot() // What is toMatchSnapshot()?
+
+  expect(states).toMatchSnapshot()
 })
